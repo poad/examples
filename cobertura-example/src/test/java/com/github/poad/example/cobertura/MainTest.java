ca2c8e6a45e51d66bc4b9ab2a95b9f853047e7eb
Kenji Saito <ken-yo@mbr.nifty.com>
2015-12-27 10:59:59 +0900

    Add dummy classes

---
+++ cobertura-example/src/test/java/com/github/poad/example/cobertura/MainTest.java
@@ -0,0 +1,22 @@
+package com.github.poad.example.cobertura;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+	@Test
+	public static void test() throws IOException {
+		Main target = new Main();
+		Arrays.asList("test", "test-data")
+				.forEach(f -> {
+					try {
+						target.execute(Arrays.asList("test1", "test2", "hoge"), f);
+					} catch (Exception e) {
+
+					}
+				});
+	}
+}
