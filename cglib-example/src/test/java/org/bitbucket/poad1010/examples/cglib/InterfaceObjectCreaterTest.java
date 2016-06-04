096540eb8edeb657f6ead370611b1e64fd01d751
Kenji Saito <ken-yo@mbr.nifty.com>
2016-06-04 10:03:43 +0900

    add the example of cglib

---
+++ cglib-example/src/test/java/org/bitbucket/poad1010/examples/cglib/InterfaceObjectCreaterTest.java
@@ -0,0 +1,22 @@
+package org.bitbucket.poad1010.examples.cglib;
+
+import org.testng.annotations.Test;
+
+public class InterfaceObjectCreaterTest {
+
+	@Test
+	public void test() {
+		TestInterface ifObj = new InterfaceObjectCreater()
+				.create(TestInterface.class, new TestInterface(){
+
+					@Override
+					public void test() {
+						System.out.println("Hello!");
+					}});
+		ifObj.test();
+	}
+
+	public static interface TestInterface {
+		void test();
+	}
+}
