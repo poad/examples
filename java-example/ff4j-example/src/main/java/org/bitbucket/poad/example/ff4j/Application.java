ca5d2c47c1bbd1922b4fbaa10fbe86415a0e6a38
Kenji Saito <ken-yo@mbr.nifty.com>
2016-11-12 14:02:44 +0900

    ff4jサンプルを動作するように修正

--- java-example/ff4j-example/src/main/java/org/bitbucket/poad/example/ff4j/Application.java
+++ java-example/ff4j-example/src/main/java/org/bitbucket/poad/example/ff4j/Application.java
@@ -2,6 +2,9 @@ package org.bitbucket.poad.example.ff4j;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.ff4j.web.bean.WebConstants;
 
 /**
  * Created by ken-yo on 2016/11/07.
@@ -10,14 +13,20 @@ public class Application {
 
     public static void main(String... args) throws Exception {
         Server server = new Server(8089);
-        ServletContextHandler context = new ServletContextHandler();
+        WebAppContext webapp = new WebAppContext();
+        webapp.setResourceBase(".");
+        ServletContextHandler context = new ServletContextHandler(
+                ServletContextHandler.NO_SESSIONS | ServletContextHandler.GZIP);
         server.setHandler(context);
-        context.setInitParameter("ff4jProvider", SampleFF4jProvider.class.getName());
-        context.addServlet(org.ff4j.web.FF4jDispatcherServlet.class, "/*");
-
+        webapp.setContextPath("/console/*");
+        context.setHandler(webapp);
+        ServletHolder servlet = new ServletHolder("console", org.ff4j.web.FF4jDispatcherServlet.class);
+        servlet.setInitParameter(WebConstants.SERVLETPARAM_FF4JPROVIDER, SampleFF4jProvider.class.getName());
+        servlet.setInitOrder(1);
+        webapp.addServlet(servlet, "/*");
 
         server.start();
 
-        server.join();
     }
+
 }
