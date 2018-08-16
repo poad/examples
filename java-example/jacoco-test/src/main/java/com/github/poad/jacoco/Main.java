28129b9aa948d66ff5f443874de54e022f3ef6bf
Kenji Saito <ken-yo@mbr.nifty.com>
2018-08-17 01:54:34 +0900

    jacoco のテストプロジェクトを追加

---
+++ java-example/jacoco-test/src/main/java/com/github/poad/jacoco/Main.java
@@ -0,0 +1,12 @@
+package com.github.poad.jacoco;
+
+public class Main {
+
+    public static void main(String... args) {
+        boolean a = true;
+        int b = a ?
+                1 :
+                2;
+        System.out.println(b);
+    }
+}
