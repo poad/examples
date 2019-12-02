baab8cfb529b6d8b8a0dbf92cb57e3c7d8fef466
Kenji Saito <ken-yo@mbr.nifty.com>
2019-12-03 04:51:37 +0900

    Fix the warn (#12)

--- java-example/morphological-analyis/kuromoji/app/src/main/java/com/github/poad/examples/kuromori/KuromoriApplication.java
+++ java-example/morphological-analyis/kuromoji/app/src/main/java/com/github/poad/examples/kuromori/KuromoriApplication.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.databind.Module;
 @SpringBootApplication
 public class KuromoriApplication implements HealthIndicator {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(KuromoriApplication.class, args);
 	}
 
--- java-example/morphological-analyis/kuromoji/app/src/test/java/com/github/poad/examples/kuromori/KuromoriApplicationTests.java
+++ java-example/morphological-analyis/kuromoji/app/src/test/java/com/github/poad/examples/kuromori/KuromoriApplicationTests.java
@@ -3,11 +3,15 @@ package com.github.poad.examples.kuromori;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class KuromoriApplicationTests {
 
 	@Test
 	void contextLoads() {
+        KuromoriApplication.main();
+        assertTrue(true);
 	}
 
 }
