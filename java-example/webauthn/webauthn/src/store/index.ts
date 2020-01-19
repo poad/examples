9ab5da890338c465c6e40de06dfd25477869035e
Kenji Saito <ken-yo@mbr.nifty.com>
2020-01-19 12:39:08 +0900

    細かい設定を見直し

--- java-example/webauthn/webauthn/src/store/index.ts
+++ java-example/webauthn/webauthn/src/store/index.ts
@@ -2,10 +2,10 @@ import { GetterTree, ActionTree, MutationTree } from 'vuex'
 import { getAccessorType } from 'typed-vuex'
 
 export enum WebAuthnPage {
-  Index = "Index",
-  SignUp = "SignUp",
-  SignIn = "SignIn",
-  Inspire = "Inspire"
+  Index = 'Index',
+  SignUp = 'SignUp',
+  SignIn = 'SignIn',
+  Inspire = 'Inspire'
 }
 
 export const state = () => ({
@@ -15,17 +15,17 @@ export const state = () => ({
 export type RootState = ReturnType<typeof state>
 
 export const getters: GetterTree<RootState, RootState> = {
-  name: state => state.page,
+  name: (_) => _.page
 }
 
 export const mutations: MutationTree<RootState> = {
-  CHANGE_PAGE: (state, page: WebAuthnPage) => (state.page = page),
+  CHANGE_PAGE: (state, page: WebAuthnPage) => (state.page = page)
 }
 
 export const actions: ActionTree<RootState, RootState> = {
   changePage({ commit }, page: WebAuthnPage) {
     commit('CHANGE_PAGE', page)
-  },
+  }
 }
 
 // This compiles to nothing and only serves to return the correct type of the accessor
@@ -36,5 +36,5 @@ export const accessorType = getAccessorType({
   actions,
   modules: {
     // The key (submodule) needs to match the Nuxt namespace (e.g. ~/store/submodule.ts)
-  },
+  }
 })
