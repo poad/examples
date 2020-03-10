a537417ff75b265619eed03ac8b39e6324669b0b
Kenji Saito <ken-yo@mbr.nifty.com>
2020-03-11 05:50:04 +0900

    Bump same

--- micronaut-examples/micronaut-native-server/docker-build.sh
+++ micronaut-examples/micronaut-native-server/docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker build . -t micronaut-server
-echo
-echo
-echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 micronaut-server"
+
+./gradlew assemble && \
+docker build . -t micronaut-native && \
+echo && \
+echo && \
+echo "To run the docker container execute:" && \
+echo "    $ docker run -p 8080:8080 micronaut-native"
