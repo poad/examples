cb9c0f54f571c7efa756d7dda4210af403787b11
Kenji Saito <ken-yo@mbr.nifty.com>
2020-08-02 09:43:03 +0900

    Feaure/mp example (#38)
    * Added the Quarkus with spring boot/spring data jpa extentions
    
    * Update the spring boot

---
+++ quarkus-example/quarkus-mp-example/src/native-test/java/com/github/poad/NativeMessageResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.poad;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMessageResourceIT extends MessageResourceTest {
+
+    // Execute the same tests but in native mode.
+}
