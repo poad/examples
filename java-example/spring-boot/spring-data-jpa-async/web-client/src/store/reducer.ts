cd4dff2aaf34837d268dfd70d1669b10e2018274
Kenji Saito <ken-yo@mbr.nifty.com>
2021-08-13 08:39:19 +0900

    Remove redux (#114)

--- java-example/spring-boot/spring-data-jpa-async/web-client/src/store/reducer.ts
+++
@@ -1,9 +0,0 @@
-import { reducerWithInitialState } from 'typescript-fsa-reducers';
-import { State } from './comment/types';
-
-export const initialState: State = {
-  comments: [],
-  comment: '',
-};
-
-export const Reducer = reducerWithInitialState(initialState);
