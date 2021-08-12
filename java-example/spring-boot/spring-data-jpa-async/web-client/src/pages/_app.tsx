2cebda44f5cc5c688441354c55c4e645eb09465b
Kenji Saito <ken-yo@mbr.nifty.com>
2021-08-13 07:51:55 +0900

    Remove redux

--- java-example/spring-boot/spring-data-jpa-async/web-client/src/pages/_app.tsx
+++ java-example/spring-boot/spring-data-jpa-async/web-client/src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
-import withRedux from 'next-redux-wrapper';
 import * as React from 'react';
-import { Provider } from 'react-redux';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { useEffect } from 'react';
-import { store, makeStore } from '../store';
 import theme from '../styles/theme';
 
 interface MyAppProps extends AppInitialProps, AppProps {}
 
-const MyApp = (props: MyAppProps) => {
+const MyApp = (props: MyAppProps): JSX.Element => {
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -22,20 +19,18 @@ const MyApp = (props: MyAppProps) => {
 
   const { Component, pageProps } = props;
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <Head>
-          <title>Home</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        {/* ThemeProvider makes the theme available down the React
-            tree thanks to React context. */}
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* ThemeProvider makes the theme available down the React
+          tree thanks to React context. */}
 
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 };
 
@@ -48,4 +43,4 @@ App.getInitialProps = async ({ Component, ctx }: Context) => {
   return { pageProps };
 };
 
-export default withRedux(makeStore)(MyApp);
+export default MyApp;
