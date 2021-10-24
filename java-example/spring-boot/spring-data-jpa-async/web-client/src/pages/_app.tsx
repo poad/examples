b5b582d36e721cad49d52fcc4ccc5a57d4d70bd3
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-10-24 12:20:27 +0900

    Migrate material-ui to mui/material (#728)

--- java-example/spring-boot/spring-data-jpa-async/web-client/src/pages/_app.tsx
+++ java-example/spring-boot/spring-data-jpa-async/web-client/src/pages/_app.tsx
@@ -1,46 +1,28 @@
 import * as React from 'react';
-import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { useEffect } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from '../styles/theme';
 
-interface MyAppProps extends AppInitialProps, AppProps {}
-
-const MyApp = (props: MyAppProps): JSX.Element => {
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  }, []);
-
-  const { Component, pageProps } = props;
-  return (
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* ThemeProvider makes the theme available down the React
+const App = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    {/* ThemeProvider makes the theme available down the React
           tree thanks to React context. */}
 
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
+
+/* eslint-disable no-restricted-globals */
+App.onRedirectCallback = (appState: { targetUrl: string }): void => {
+  history.state.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname);
 };
+/* eslint-enable no-restricted-globals */
 
-type Context = AppContext;
-
-App.getInitialProps = async ({ Component, ctx }: Context) => {
-  const pageProps = Component.getInitialProps
-    ? await Component.getInitialProps(ctx)
-    : {};
-  return { pageProps };
-};
-
-export default MyApp;
+export default App;
